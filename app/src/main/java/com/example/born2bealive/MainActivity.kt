dffee34124ba1a9d2e883be0dc103212f7ca5b14
jerome <jerome.laquay@hotmail.fr>
2019-03-03 22:16:15 +0100

    :sparkles: bottom menu

---
+++ app/src/main/java/com/example/born2bealive/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.born2bealive
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/born2bealive/TodolistFragment.java
@@ -0,0 +1,23 @@
+package com.example.born2bealive;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TodolistFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_todolist, null);
+
+
+        return view;
+    }
+
+
+}
